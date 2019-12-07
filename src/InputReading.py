2ec976cd5b430961f325dd87c83c885d538534dc
Ao Dong <53056165+Ao99@users.noreply.github.com>
2019-12-07 18:48:07 -0500

    Update implemented InputReading module

--- src/InputReading.py
+++ src/InputReading.py
@@ -6,7 +6,7 @@ import os
 def inputArray():
     inputData = DataStructure()
     
-    filepath = "/home/ubuntu/environment/Peter Module 3/input.txt"
+    filepath = "/input/input.txt"
     if os.path.exists(filepath):
         try:
             fh = open(filepath)
@@ -25,6 +25,3 @@ def inputArray():
             print("Error: Could not read the input file.")
     else:
         print("Input file not found.")
-    
-inputArray()
-
