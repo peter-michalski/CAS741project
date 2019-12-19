79060b2460b335f099d9410a3d2f8e0dca1458d5
peter <michap@mcmaster.ca>
2019-12-18 19:14:51 -0500

    small updates m2

--- src/M2SystemControl.py
+++ src/M2SystemControl.py
@@ -23,25 +23,27 @@ def main_function():
             problem_parameters = format_input(input_data)
             boundary_data = get_boundary(input_data)
 
-            # perform the simulation (based on pylbm tutorial), iterating for for time intervals - this is M5, M6, M7
+            # This is M5, M6, M7; perform the simulation (based on pylbm tutorial - https://pylbm.readthedocs.io/en/latest/notebooks/07_Von_Karman_vortex_street.html), iterating for for time intervals
             solution = pylbm.Simulation(problem_parameters)
             while solution.t < input_data["Time"]:
                 solution.one_time_step()
 
-            # create the image (based on pylbm tutorial) - this is M11
+            # This is M11 - create the image (also based on pylbm tutorial)
             viewer = pylbm.viewer.matplotlib_viewer
             fig = viewer.Fig()
-            ax = fig[0]
+            image = fig[0]
+
             def vorticity(solution):
                 ux = solution.m[qx] / solution.m[rho]
                 uy = solution.m[qy] / solution.m[rho]
-                V = np.abs(uy[2:, 1:-1] - uy[0:-2, 1:-1] - ux[1:-1, 2:] + ux[1:-1, 0:-2]) / (2 * solution.domain.dx)
-                return -V
-            im = ax.image(vorticity(solution).transpose(), clim=[-3., 0])
-            ax.ellipse([.3 / (1./64), 0.5 * (boundary_data["y_min"] + boundary_data["y_max"]) / (1./64)], [0.05 / (1./64), 0.05 / (1./64)], 'r')
-            ax.title = 'Von Karman vortex street at t = {0:f}'.format(solution.t)
+                vortvector = np.abs(uy[2:, 1:-1] - uy[0:-2, 1:-1] - ux[1:-1, 2:] + ux[1:-1, 0:-2]) / (2 * solution.domain.dx)
+                return -vortvector
+            im = image.image(vorticity(solution).transpose(), clim=[-3., 0])
+            image.ellipse([.3 / (1./64), 0.5 * (boundary_data["y_min"] + boundary_data["y_max"]) / (1./64)], [0.05 / (1./64), 0.05 / (1./64)], 'r')
+            image.title = 'Von Karman Vortex Street at time = {0:f}'.format(solution.t)
 
             # output the image to the screen - M1
-
             fig.show()
+
+
 main_function()
